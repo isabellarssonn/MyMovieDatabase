7dc1891426743a6776afe65a1e9ec3edd08425e4
isabellarssonn <isabel.larsson@hotmail.com>
2025-02-20 11:32:58 +0100

    klar med topplistan för 20 högst rankade filmer

--- scripts/components/movieCard.js
+++ scripts/components/movieCard.js
@@ -1,1 +1,13 @@
 // Skapa filmkort
+
+export function movieCard(movie) {
+    let movieElement = document.createElement("div");
+    movieElement.classList.add("movie-card");
+
+    movieElement.innerHTML = 
+        `<img src="${movie.Poster}" alt="Bild av filmen ${movie.Title}" class="movie-card__img">
+        <h3 class="movie-card__title">${movie.Title}</h3>`
+        ;
+
+    return movieElement;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@
 import { fetchTopMovies } from './modules/api.js'
 import { shuffledTrailers } from "./utils/utils.js";
 import { renderTrailers } from './modules/carousel.js';
+import { movieCard } from './components/movieCard.js';
 
 // console.log(await fetchTopMovies());
 
@@ -26,12 +27,21 @@ if(window.location.pathname === '/' || window.location.pathname === '/index.html
 pageSetup();
 
 async function pageSetup() {
-    let allMovies = await fetchTopMovies(); // Hämtar alla filmer
-    let movies = await shuffledTrailers(); // Hämtar slumpade filmer
+
+    // Hämtar och visar slumpade trailers
+    let movies = await shuffledTrailers();
 
     document.querySelector('.trailers__container').innerHTML = ""; 
 
     movies.forEach((movie, index) => {
         renderTrailers(movie, index + 1);
     });
+
+    // Hämtar filmer, slicear 20, lägger till filmkort i HTML element
+    let allMovies = await fetchTopMovies(); // Hämtar alla filmer
+    let highRankMovies = allMovies.slice(0, 20)  
+    let sectionRef = document.querySelector('#cardContainer');
+    for(let movie of highRankMovies) {
+        sectionRef.appendChild(movieCard(movie))
+    }
 }
